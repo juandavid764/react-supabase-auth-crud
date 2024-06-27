741838c4dc8061fc49cdfb97a305eb533d437e81
Sebastian <semega1206@gmail.com>
2024-06-27 15:52:23 -0500

    Edit home test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,22 @@
-import React, { useEffect } from 'react';
-import { supabase } from '../supabase/client';
-import { useNavigate } from 'react-router-dom';
-import TaskFrom from '../components/TaskFrom';
+import React, { useEffect } from "react";
+import { supabase } from "../supabase/client";
+import { useNavigate } from "react-router-dom";
+import TaskFrom from "../components/TaskFrom";
 
 export default function Home() {
-
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!supabase.auth.getUser()) {
       navigate("/login");
     }
-
-  }, [navigate])
+  }, [navigate]);
 
   return (
     <div>
-      Home
-      <button onClick={() => supabase.auth.signOut()}>
-        Logout
-      </button>
-
-      <TaskFrom/>
+      Home sebastian
+      <button onClick={() => supabase.auth.signOut()}>Logout</button>
+      <TaskFrom />
     </div>
-  )
+  );
 }
